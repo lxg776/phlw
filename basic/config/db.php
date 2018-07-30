4d0548ca11bfc35d3e7767d48f1931410dcb7897
lxg776 <262621979@qq.com>
2018-07-30 10:28:12 +0800

    first commit

---
+++ basic/config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=222.83.250.251;dbname=web6_gxbs_net',
+    'username' => 'web6_gxbs_net',
+    'password' => 'lxg123321',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
---
+++ basic/models/UcenterUser.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/7/23 0023
+ * Time: 上午 10:41
+ */
+
+namespace app\models;
+use yii\db\ActiveRecord;
+
+class UcenterUser extends  ActiveRecord
+{
+
+}
